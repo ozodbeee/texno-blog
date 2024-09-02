63ad53080031f5ad7461a31e6b9123e976c1caab
OzodbekMuhtorjonov31 <ozodbekmuhtorjonov08@gmail.com>
2024-09-02 11:06:26 +0500

    Uploaded

--- app/blog/[name]/page.tsx
+++ app/blog/[name]/page.tsx
@@ -65,7 +65,7 @@ export default async function BlogIndexPage({
 			</nav>
 
 			<div className='grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-3 lg:gap-7'>
-				{data.posts.map(item => (
+				{data.posts.map((item: any) => (
 					<Card key={item.id}>
 						<Image
 							src={item.image ?? Defaultimage}
